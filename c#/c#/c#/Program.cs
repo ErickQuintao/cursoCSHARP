cd80575215bb62831f21ac9807b36fbf360f640f
Erick Quintao <erickquintao@outlook.com>
2023-10-18 18:04:13 -0300

    adicionando

--- c#/c#/c#/Program.cs
+++ c#/c#/c#/Program.cs
@@ -5,7 +5,8 @@
         
         static void Main(string[] args)
         {
-          
+           
         }
+       
     }
 }
---
+++ c#/c#/c#/objs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_
+{
+    internal class objs
+    {
+        /*
+          int v1, v2;
+            v1=Convert.ToInt32(Console.ReadLine());
+            v2=Convert.ToInt32(Console.ReadLine());
+            soma(v1, v2);
+         */
+    }
+    /* static void soma(int n1, int n2)
+     {
+         int res = n1 + n2;
+         Console.WriteLine("A Soma de {0} e {1} é: {2}", n1, n2, res);
+     }*/
+}
