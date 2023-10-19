cb07125996bd6001c3f0204fb0c94690ba1c46a9
Erick Quintao <erickquintao@outlook.com>
2023-10-19 18:46:30 -0300

    restruturando

---
+++ c#/ArgumentParams.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace aulas
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            soma(43,5435,76,45,25,2,5);
+        }
+      
+       static void soma(params int[]n)
+        {
+            int res =0;
+            if(n.Length < 1)
+            {
+                Console.WriteLine("Não existem valores a serem somados");
+            } else if(n.Length < 2)
+            {
+                Console.WriteLine("Valores insuficientes para soma: {0}", n[0]);
+            }
+            else
+            {
+                for(int i=0;i<n.Length;i++)
+                {
+                    res += n[i];
+                }
+                Console.WriteLine("A soma dos valores é: {0}", res);
+            }
+        }
+    }
+}
