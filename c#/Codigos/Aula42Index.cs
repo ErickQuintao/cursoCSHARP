35271c9d0d9c80c35e4a3fc0de633c784c587395
Erick Quintao <erickquintao@outlook.com>
2023-10-20 19:39:45 -0300

    add aula42

---
+++ c#/Codigos/Aula42Index.cs
@@ -0,0 +1,44 @@
+using System;
+/*
+namespace Aula
+{
+    class Carro
+    {
+        private int[] velMax = new int[5] { 8, 645, 5435, 543, 4 };
+
+        public int this[int i]
+        {
+            get { return velMax[i]; }
+            set
+            {
+                if (value < 0)
+                {
+                    velMax[i] = 0;
+                }
+                else if (value > 300)
+                {
+                    velMax[i] = 300;
+                }
+                else
+                {
+                    velMax[i] = value;
+                }
+            }
+        }
+        public Carro()
+        {
+
+        }
+    }
+    class Aula41
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro();
+            c1[4] = 200;
+            Console.WriteLine(c1[4]);
+        }
+    }
+
+}
+*/
