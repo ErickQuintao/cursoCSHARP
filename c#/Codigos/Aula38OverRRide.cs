5e460dc9607f69d76791dd2783177522c463f062
Erick Quintao <erickquintao@outlook.com>
2023-10-20 18:11:53 -0300

    add aula38

---
+++ c#/Codigos/Aula38OverRRide.cs
@@ -0,0 +1,37 @@
+using System;
+/*
+namespace Aula
+{
+    class Base
+    {
+
+
+        virtual public void info()
+        {
+            Console.WriteLine("base!");
+        }
+    }
+    class Derivada1 : Base
+    {
+        public Derivada1()
+        {
+            Console.WriteLine("Construtor da classe Derivada 1");
+        }
+        override public void info()
+        {
+            Console.WriteLine("Derivadoaaa");
+        }
+    }
+    class Aula38
+    {
+        static void Main(string[] args)
+        {
+            Base Ref;
+            Derivada1 derivada1 = new Derivada1();
+            Ref = derivada1;
+            Ref.info();
+        }
+    }
+
+}
+*/
--- c#/Codigos/cast.cs
+++ c#/Codigos/cast.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
 namespace c_
 {
-    internal class cast
-    {
-        /*
-          float n1 = 10.6f;
-            int n2 = (int)n1;//type cast
-            Console.WriteLine(n2);
-         */
-    }
+   internal class cast
+   {
+
+         float n1 = 10.6f;
+           int n2 = (int)n1;//type cast
+           Console.WriteLine(n2);
+
+   }
 }
+ */
