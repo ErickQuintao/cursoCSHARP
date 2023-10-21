48aee68c0d0bc27e6a46c93dbcba1f35e31b289b
Erick Quintao <erickquintao@outlook.com>
2023-10-21 17:57:16 -0300

    add aula43 e restrurei o projs

---
+++ c#/Codigos/Aula43Interf.cs
@@ -0,0 +1,47 @@
+using System;
+/*
+namespace Aula
+{
+    public interface Veiculo
+    {
+        void ligar();
+        void desligar();
+        void info();
+    }
+    public interface Combate
+    {
+        void disparar();
+    }
+    class Carro : Veiculo, Combate
+    {
+        public bool ligado;
+        private int municao;
+        public Carro()
+        {
+            setMunicao(100);
+        }
+        public void setMunicao(int qtde)
+        {
+            this.municao = qtde;
+        }
+        public void ligar()
+        {
+            this.ligado = true;
+        }
+        public void desligar()
+        {
+            this.ligado = false;
+        }
+        public void disparar() { }
+        public void info() { }
+    }
+    class Aula43
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
+*/
